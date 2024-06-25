e13e132fc5fcb35e09ae2f6b35fbcd6068711740
mikhailm <mikmaksi@gmail.com>
2024-06-25 14:21:52 -0700

    agglomerative transformer starting to work

--- ds_utils/dimreduce/feature_selection.py
+++ ds_utils/dimreduce/feature_selection.py
@@ -24,254 +24,6 @@ from sklearn.utils._param_validation import Interval
 from sklearn.utils.validation import check_is_fitted
 
 
-@dataclass
-class MetricCluster:
-    cluster_id: int
-    selected: str
-    all_metrics: list[str]
-
-
-@dataclass
-class ClusteringStep:
-    step: int
-    model: AgglomerativeClustering = None
-    metric_clusters: list[MetricCluster] = field(default_factory=list)
-    linkage_matrix = None
-    leaf_order = None
-
-    @property
-    def selected_metrics(self) -> list[str]:
-        # put selected metrics into a list
-        mets = [c.selected for c in self.metric_clusters]
-
-        # sort the metrics by the leaf order from clusterin
-        # note: metrics is a subset of leaf order
-        mets = sorted(mets, key=lambda x: self.leaf_order.index(x))
-        return mets
-
-    def cluster(self, X: pd.DataFrame, r: float = 0.9) -> None:
-        """Cluster the features in X based n Pearson correlation
-
-        Args:
-            r (float): Pearson correlation coefficient threshold for similarity (0, 1).
-
-        Returns:
-            None:
-        """
-        # calculate the distance matrix using Pearson correlation
-        dist_mat = 1 - X.corr().abs()
-        dist_mat[dist_mat < 0] = 0
-
-        # cluster based on precomputed distance matrix
-        self.model = AgglomerativeClustering(
-            distance_threshold=1 - r, n_clusters=None, metric="precomputed", linkage="complete"
-        )
-        self.model.fit(dist_mat)
-
-        # set the linkage matrix and link order
-        self.set_linkage_matrix(self.model)
-        self.leaf_order = X.columns[leaves_list(self.linkage_matrix)].tolist()
-
-    def select(self, X: pd.DataFrame, y: pd.Series, score_func: callable = f_classif) -> None:
-        """Select the single best metric from each cluster based on a univariate statistical test.
-
-        Args:
-            score_func (callable): one of a list of sklearn support scoring functions for classification
-                chi2, f_classif, mutual_info_classif
-
-        Returns:
-            None:
-        """
-
-        for cluster_id in np.unique(self.model.labels_):
-            selector = SelectKBest(score_func, k=1)
-            selector.fit(X.iloc[:, np.where(self.model.labels_ == cluster_id)[0]], y)
-            self.metric_clusters.append(
-                MetricCluster(
-                    cluster_id=cluster_id,
-                    selected=selector.get_feature_names_out()[0],
-                    all_metrics=selector.feature_names_in_.tolist(),
-                )
-            )
-
-    def set_linkage_matrix(self, model: AgglomerativeClustering, **kwargs) -> None:
-        """Create a matrix representing the linkage from an AgglomerativeClustering model
-            source: https://scikit-learn.org/stable/auto_examples/cluster/plot_agglomerative_dendrogram.html
-
-        Args:
-            model (cluster.AgglomerativeClustering): an agglomerative clustering model
-        """
-        # create the counts of samples under each node
-        counts = np.zeros(model.children_.shape[0])
-        n_samples = len(model.labels_)
-        for i, merge in enumerate(model.children_):
-            current_count = 0
-            for child_idx in merge:
-                if child_idx < n_samples:
-                    current_count += 1  # leaf node
-                else:
-                    current_count += counts[child_idx - n_samples]
-            counts[i] = current_count
-
-        self.linkage_matrix = np.column_stack([model.children_, model.distances_, counts]).astype(float)
-
-    def get_correlated_pairs(self) -> list[tuple]:
-        # make a list of pairs to plot
-        pairs = []
-        for metric_cluster in self.metric_clusters:
-            if len(metric_cluster.all_metrics) > 1:
-                pairs.extend(list(combinations(metric_cluster.all_metrics, 2)))
-        return pairs
-
-
-class AgglomerativeSelector(SelectorMixin, BaseEstimator):
-    def __init__(self, r: float = 0.9, score_func: callable = f_classif):
-        """__init__.
-
-        Args:
-            X (pd.DataFrame): a DataFrame of predictors optinally indexed by observation index
-            y (pd.Series): as Series of y labels optionally indexed by observation index
-        """
-
-        # inputs
-        self.r = r
-        self.score_func = score_func
-
-        # outputs
-        self.clustering_steps_: list[ClusteringStep] = []
-        self.fig_diag = None
-
-    def fit(self, X, y) -> None:
-        """Main loop of iterative dimensionality reduction
-
-        Args:
-            r (float): threshold for Pearson correlation coefficient
-            score_func (callable): score_func to be passed to the feature selector
-
-        Returns:
-            None:
-        """
-        # check that pandas output was set
-        validate_transform_output(self)
-
-        # validate data to be able to use .get_feature_names_out
-        X = self._validate_data(
-            X,
-            accept_sparse=("csr", "csc"),
-            dtype=np.float64,
-            force_all_finite="allow-nan",
-        )
-
-        # transform back to DataFrame
-        X = pd.DataFrame(X, columns=self.feature_names_in_)
-
-        i = 1
-        not_done = True
-        X_r = X.copy()
-        while not_done:
-            # print(f"Running clustering loop: {i}")
-
-            # create a clustering step
-            clustering_step = ClusteringStep(step=i)
-
-            # cluster features
-            clustering_step.cluster(X_r, self.r)
-
-            # select the best features from each cluster
-            clustering_step.select(X_r, y, self.score_func)
-
-            # add to list
-            self.clustering_steps_.append(clustering_step)
-
-            # reduce input X
-            X_r = X_r[self.get_feature_names_out()]
-
-            # check if complete
-            not_done = np.any(np.triu(X_r.corr().abs(), k=1) > self.r)
-            i += 1
-        return self
-
-    def _get_support_mask(self):
-        if self.clustering_steps_ == []:
-            mask = self.feature_names_in_ == self.feature_names_in_
-        else:
-            selected_metrics = self.clustering_steps_[-1].selected_metrics
-            mask = np.isin(self.feature_names_in_, selected_metrics)
-        return mask
-
-    @property
-    def filtered_features_(self):
-        check_is_fitted(self)
-        return np.array(list(set(self.feature_names_in_) - set(self.get_feature_names_out())))
-
-    def plot_clust_heatmaps(self, X) -> Figure:
-        """Create diagnostic plot with a clustering dendrogram and correlation heatmap.
-
-        Args:
-            None:
-
-        Returns:
-            None:
-        """
-        # create figure
-        fig, axs = plt.subplots(nrows=3, ncols=1, height_ratios=(1, 0.8, 0.8))
-
-        # visualize the dendrogram for the initial clustering
-        step1 = self.clustering_steps_[0]
-        dendrogram(
-            step1.linkage_matrix,
-            ax=axs[0],
-            color_threshold=1 - self.r,
-            orientation="right",
-            labels=X.columns,
-        )
-        fig.tight_layout()
-
-        # visualize the heatmap for non reduced X
-        sns.heatmap(X.corr().loc[step1.leaf_order, step1.leaf_order], ax=axs[1])
-
-        # visualize the correlation map of reduced X for the final step
-        # note: selected_metrics are in leaf order
-        last_step = self.clustering_steps_[-1]
-        sns.heatmap(
-            X.corr().loc[last_step.selected_metrics, last_step.selected_metrics],
-            ax=axs[2],
-        )
-
-        # set figure size
-        fig.set_size_inches((6, 10))
-        return fig
-
-    def plot_bivariate(self, X, correlated: bool = True) -> Figure:
-        """Make a list of pairs to plot of correlated pairs from each clustering step."""
-        if correlated:
-            pairs = sum([step.get_correlated_pairs() for step in self.clustering_steps_], [])
-        else:
-            pairs = list(combinations(self.get_feature_names_out(), r=2))
-
-        # create a plot
-        nrows = ceil(sqrt(len(pairs)))
-        if nrows != 0:
-            fig, axs = plt.subplots(nrows=nrows, ncols=ceil(len(pairs) / nrows))
-            if isinstance(axs, Axes):
-                pair = pairs[0]
-                sns.scatterplot(data=X[list(pair)], x=pair[0], y=pair[1], ax=axs)
-            else:
-                for i, (pair, ax) in enumerate(zip(pairs, axs.flat)):
-                    sns.scatterplot(data=X[list(pair)], x=pair[0], y=pair[1], ax=ax)
-            fig.tight_layout()
-        else:
-            fig, axs = plt.subplots()
-        return fig
-
-    def plot_pairs(self, X, color: pd.Series = None) -> sns.PairGrid:
-        g = sns.PairGrid(X)
-        g.map_diag(sns.histplot)
-        g.map_lower(sns.scatterplot, hue=color)
-        g.map_upper(plot_r_coef)
-        return g
-
-
 class ScaledVarianceThreshold(SelectorMixin, BaseEstimator):
     _parameter_constraints: dict = {"threshold": [Interval(Real, 0, None, closed="left")]}
 
@@ -320,6 +72,11 @@ class ScaledVarianceThreshold(SelectorMixin, BaseEstimator):
 class CorrFeatureAgglomeration(FeatureAgglomeration):
     @staticmethod
     def pool_by_pca(X, axis=1):
+        # if a single feature comes in, return it directly
+        if X.shape[1] == 1:
+            return X.reshape(X.shape[0])
+
+        # if multiple features, return the first PCA component
         reduced = PCA(n_components=1).fit_transform(X).to_numpy()
         return reduced.reshape(reduced.shape[0])
 
@@ -340,9 +97,14 @@ class CorrFeatureAgglomeration(FeatureAgglomeration):
         )
 
     def fit(self, X, y=None):
-        corr_mat = 1 - X.corr().abs()
-        super().fit(corr_mat)
+        dist_mat = 1 - X.corr().abs()
+        dist_mat[dist_mat < 0] = 0
+        super().fit(dist_mat)
         return self
+
+    @property
+    def leaf_order(self):
+        return self.feature_names_in_[leaves_list(self.linkage_matrix)].tolist()
 
     @property
     def linkage_matrix(self) -> np.ndarray:
@@ -367,20 +129,79 @@ class CorrFeatureAgglomeration(FeatureAgglomeration):
         return np.column_stack([self.children_, self.distances_, counts]).astype(float)
 
 
+@dataclass
+class MetricCluster:
+    cluster_id: int
+    selected: str
+    all_metrics: list[str]
+
+
+@dataclass
+class ClusteringStep:
+    step_num: int
+    model: CorrFeatureAgglomeration
+    metric_clusters: list[MetricCluster] = field(default_factory=list)  # set during fit
+
+    @property
+    def r(self) -> float:
+        return self.model.r
+
+    @property
+    def linkage_matrix(self) -> np.ndarray:
+        return self.model.linkage_matrix
+
+    @property
+    def leaf_order(self) -> list:
+        return self.model.leaf_order
+
+    @property
+    def correlated_pairs(self) -> list[tuple]:
+        # make a list of pairs to plot
+        pairs = []
+        for metric_cluster in self.metric_clusters:
+            if len(metric_cluster.all_metrics) > 1:
+                pairs.extend(list(combinations(metric_cluster.all_metrics, 2)))
+        return pairs
+
+    def fit(self, X) -> None:
+        # fit the model
+        self.model.fit(X)
+
+        # set metric clusters
+        for i, cluster_id in enumerate(np.unique(self.model.labels_)):
+            self.metric_clusters.append(
+                MetricCluster(
+                    cluster_id=cluster_id,
+                    selected=self.model.get_feature_names_out()[i],
+                    all_metrics=X.columns[self.model.labels_ == cluster_id],
+                )
+            )
+
+    def transform(self, X) -> None:
+        return self.model.transform(X)
+
+
 class IterativeCorrFeatureAgglomeration(BaseEstimator, TransformerMixin):
     def __init__(self, r: float = 0.9):
         self.r = r
-        self.steps = []
+        self.steps: list[ClusteringStep] = []
 
     def fit(self, X, y=None):
-        not_done = True
+        is_correlated = True
         Xr = X.copy()
-        while not_done:
-            step = CorrFeatureAgglomeration(r=self.r)
+        step_num = 1
+        while is_correlated:
+            # run step
+            step = ClusteringStep(step_num, CorrFeatureAgglomeration(r=self.r))
             step.fit(Xr)
             Xr = step.transform(Xr)
-            not_done = np.any(np.triu(Xr.corr().abs(), k=1) > self.r)
+
+            # check remaining correlation
+            is_correlated = np.any(np.triu(Xr.corr().abs(), k=1) > self.r)
+
+            # update steps
             self.steps.append(step)
+            step_num += 1
         return self
 
     def transform(self, X, y=None):
@@ -394,8 +215,7 @@ class FeatureAgglomerationDisplay:
     def __init__(
         self, X, transformer: Union[Union[CorrFeatureAgglomeration, IterativeCorrFeatureAgglomeration], Pipeline]
     ):
-        """If PIpeline then Agglomeration transformer must be the last step
-        """
+        """If PIpeline then Agglomeration transformer must be the last step"""
 
         # set attributes
         self.X = X
@@ -406,24 +226,22 @@ class FeatureAgglomerationDisplay:
         else:
             self.transformer = transformer
 
-    def get_cluster_step(self, step_num: int = None):
-        if isinstance(self.transformer, IterativeCorrFeatureAgglomeration):
-            if step_num is None:
-                raise Exception("'step_num' required with IterativeCorrFeatureAgglomeration")
-            return self.transformer.steps[step_num]
-        else:
-            if step_num is not None:
-                raise Exception("'step_num' not useable with CorrFeatureAgglomeration")
-            return self.transformer
-
     @property
     def _X(self):
-        if self.preroc is not None:
+        if self.preproc is None:
             return self.X
         else:
             return self.preproc.transform(self.X)
 
-    def plot_dendrogram(self, step_num: int = None) -> Figure:
+    def get_step(self, step_num: int = 0):
+        if isinstance(self.transformer, IterativeCorrFeatureAgglomeration):
+            return self.transformer.steps[step_num]
+        else:
+            if step_num > 0:
+                raise Exception("CorrFeatureAgglomeration is a single step transformer.")
+            return self.transformer
+
+    def plot_dendrogram(self, step_num: int = 0) -> Figure:
         """Create diagnostic plot with a clustering dendrogram and correlation heatmap.
 
         Args:
@@ -432,28 +250,38 @@ class FeatureAgglomerationDisplay:
         Returns:
         """
         fig, ax = plt.subplots()
-        step = self.get_cluster_step(step_num)
+        step: ClusteringStep = self.get_step(step_num)
         dendrogram(
             step.linkage_matrix,
             ax=ax,
             color_threshold=1 - step.r,
             orientation="right",
-            # labels=self.X.columns,
+            labels=step.leaf_order,
         )
         fig.tight_layout()
         return fig
 
-    def plot_in_corr_map(self, X) -> Figure:
+    def plot_in_corr_map(self, step_num: int = 0) -> Figure:
+        # apply preprocess if necessary
+        X = self._X.copy()
+
+        # transform if more than one step
+        for i in range(step_num):
+            X = self.get_step(i).transform(X)
         fig = sns.clustermap(X.corr(), annot=True, fmt=".2f")
         return fig
 
-    def plot_out_corr_map(self, X) -> Figure:
-        Xr = self.transform(X)
-        fig = sns.clustermap(Xr.corr(), annot=True, fmt=".2f")
+    def plot_out_corr_map(self) -> Figure:
+        # apply preprocess if necessary
+        X = self.transformer.transform(self._X)
+        fig = sns.clustermap(X.corr(), annot=True, fmt=".2f")
         return fig
 
     def plot_bivariate(self, X, correlated: bool = True) -> Figure:
-        """Make a list of pairs to plot of correlated pairs from each clustering step."""
+        """
+        TODO: this has not been adapted or tested
+        Make a list of pairs to plot of correlated pairs from each clustering step.
+        """
         if correlated:
             pairs = sum([step.get_correlated_pairs() for step in self.clustering_steps_], [])
         else:
